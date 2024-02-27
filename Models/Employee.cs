eb655d62b2d0f83ebe81e8c3abb34143d33dd419
nguyenlien381 <97586322+nguyenlien381@users.noreply.github.com>
2024-02-27 22:26:18 +0700

    Delete Models directory

--- Models/Employee.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace MvcMovie.Models;
-    public class Employee : Persion
-    {
-        public string EmployeeId { get; set; }
-        public int Age { get; set; }
-    }
